c3c2216f8ac6fe94b90e70b3c847120bf325ac4e
Josh Klope <klope3@msn.com>
2023-05-02 20:26:12 -0500

    Add random character placement

--- src/redux/initialState.ts
+++ src/redux/initialState.ts
@@ -1,51 +1,10 @@
-import {
-  gameBoardCellsX,
-  gameBoardCellsY,
-  playerHealthStart,
-} from "../constants";
+import { generateCharacters } from "../generate/characters";
 import { generateCells } from "../generate/environment";
-import { Cell, CharacterData, GameState } from "../types/gameStateTypes";
-import { getNewId } from "../utility";
+import { GameState } from "../types/gameStateTypes";
 
 export function getInitialState(): GameState {
-  const initialCharacters: CharacterData[] = [
-    {
-      type: "player",
-      health: playerHealthStart,
-      curCellIndex: 26,
-      id: getNewId(),
-    },
-    {
-      type: "enemy",
-      health: 1,
-      curCellIndex: 28,
-      id: getNewId(),
-    },
-    {
-      type: "enemy",
-      health: 1,
-      curCellIndex: 20,
-      id: getNewId(),
-    },
-    {
-      type: "enemy",
-      health: 1,
-      curCellIndex: 10,
-      id: getNewId(),
-    },
-    {
-      type: "enemy",
-      health: 1,
-      curCellIndex: 34,
-      id: getNewId(),
-    },
-  ];
   const initialCells = generateCells();
-  initialCells[26].characterHere = initialCharacters[0];
-  initialCells[28].characterHere = initialCharacters[1];
-  initialCells[20].characterHere = initialCharacters[2];
-  initialCells[10].characterHere = initialCharacters[3];
-  initialCells[34].characterHere = initialCharacters[4];
+  const initialCharacters = generateCharacters(initialCells);
   const initialState: GameState = {
     activeCharacters: initialCharacters,
     selectedCellIndex: undefined,
