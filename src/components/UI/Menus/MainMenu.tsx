bcf58d585c9faf1cbb90dd554346a11352e7533f
Josh Klope <klope3@msn.com>
2023-05-10 13:18:47 -0500

    Make new game button delete existing save; add confirmation screen before player does this

--- src/components/UI/Menus/MainMenu.tsx
+++ src/components/UI/Menus/MainMenu.tsx
@@ -1,16 +1,44 @@
 import { useDispatch } from "react-redux";
 import { startPlaying } from "../../../redux/gameActions";
+import { useState } from "react";
 
 export function MainMenu() {
   const dispatch = useDispatch();
   const saveExists = localStorage.getItem("save") !== null;
-
+  const [confirmDeleteSave, setConfirmDeleteSave] = useState(false);
+  // TODO: "New Game" should open a new screen that allows the user to choose seed, world size, difficulty, etc.
   return (
     <div className="menu">
-      {saveExists && (
-        <button onClick={() => dispatch(startPlaying())}>Continue</button>
+      {!confirmDeleteSave && (
+        <>
+          {saveExists && (
+            <button onClick={() => dispatch(startPlaying())}>Continue</button>
+          )}
+          <button
+            onClick={() => {
+              if (saveExists) {
+                setConfirmDeleteSave(true);
+              }
+            }}
+          >
+            New Game
+          </button>
+        </>
       )}
-      <button onClick={() => dispatch(startPlaying())}>New Game</button>
+      {confirmDeleteSave && (
+        <>
+          <div>This will DELETE your existing save. Press "GO" to confirm.</div>
+          <button
+            onClick={() => {
+              localStorage.removeItem("save");
+              dispatch(startPlaying());
+            }}
+          >
+            GO
+          </button>
+          <button onClick={() => setConfirmDeleteSave(false)}>Cancel</button>
+        </>
+      )}
     </div>
   );
 }
